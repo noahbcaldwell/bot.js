823777eba156a7d4e43ab5711e2f825059c9afc7
noahbcaldwell <107315487+noahbcaldwell@users.noreply.github.com>
2024-04-28 13:02:37 -0500

    update

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-sudo apt-get update
+sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm xvfb x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps clang libdbus-1-dev libgtk2.0-dev libnotify-dev libgconf2-dev libasound2-dev libcap-dev libcups2-dev libxtst-dev libxss1 libnss3-dev gcc-multilib g++-multilib
 sudo mkdir ~/automation && cd ~/automation
 npm init -y
